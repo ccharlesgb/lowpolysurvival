ca6597266f4fec4b68a2e08996e3838188e8e7bb
hinton <hinton.oscar@gmail.com>
2014-04-20 15:56:05 +0200

    Created a test scenario for energy testing. Added a very basic Generator and Lamp.

---
+++ Assets/Scripts/Energy/Generator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Energy
+{
+
+	public class Generator : MonoBehaviour
+	{
+
+		public bool Active = false;
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		// Player click on the object
+		void OnMouseDown()
+		{
+			// Flip the boolean
+			this.Active = !this.Active;
+
+			if (this.Active)
+			{
+				this.Activate();
+			}
+			else
+			{
+				this.Deactivate();
+			}
+		}
+
+		void Activate()
+		{
+			this.renderer.material.color = Color.green;
+		}
+
+		void Deactivate()
+		{
+			this.renderer.material.color = Color.red;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Energy/Lamp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Energy
+{
+	public class Lamp : MonoBehaviour
+	{
+		private Generator myGenerator;
+		private Light myLight;
+
+		// Use this for initialization
+		void Start()
+		{
+			this.myLight = this.GetComponent<Light>();
+
+			var Generators = GameObject.FindGameObjectsWithTag("Generator");
+			foreach (GameObject g in Generators)
+			{
+				this.myGenerator = g.GetComponent<Generator>();
+			}
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			myLight.enabled = myGenerator.Active;
+		}
+	}
+}
