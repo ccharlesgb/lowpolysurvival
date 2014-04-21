1ddd08d17c42f30e19d46725a0bbce4dc0e96207
hinton <hinton.oscar@gmail.com>
2014-04-21 23:18:05 +0200

    Added CameraController C# script. Added player prefab.

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Player
+{
+	public class CameraController : MonoBehaviour
+	{
+
+		// Player object to follow.
+		public GameObject player;
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+
+}
