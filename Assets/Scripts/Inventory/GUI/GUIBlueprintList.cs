d09b79cf3f9636fe86a55987e3b47b11e4b54002
hinton <hinton.oscar@gmail.com>
2014-07-27 01:45:11 +0200

    Started on the Blueprint GUI.

---
+++ Assets/Scripts/Inventory/GUI/GUIBlueprintList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using LowPolySurvival.Inventory.Blueprints;
+using UnityEngine;
+
+namespace LowPolySurvival.Inventory
+{
+	internal class GUIBlueprintList : GUIElement
+	{
+
+		private BlueprintList _blueprintList;
+
+		// Use this for initialization
+		public GUIBlueprintList(IGUIElement parentElement, GUIPosition position) : base(parentElement, position)
+		{
+			// Load the global blueprint list.
+			_blueprintList = Blueprints.MasterList.Instance.List;
+
+			WindowRect = new Rect(0, 0, 200, 200);
+		}
+
+		public override void Draw()
+		{
+			for (int i = 0; i < _blueprintList.List.Count; i++)
+			{
+				Blueprint blueprint = _blueprintList.List[i];
+
+				var rect = new Rect(WindowRect.x + 20*i, WindowRect.y, 100, 20);
+				GUI.Label(rect, blueprint.PrintName);
+			}
+		}
+	}
+}
